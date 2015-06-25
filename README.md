87ddf326e95b5110bcaafdd6e89ef44cd697d6ab
roberto <roberto@golgi.local>
2015-06-25 06:46:40 +0200

    change from C to JS

--- README.md
+++
@@ -1,4 +0,0 @@
-growth
-======
-
-Growth
--- foldlength.sh
+++
@@ -1,5 +0,0 @@
-for name in $(cat log2/name.txt)
-do
-	name=$name
-	cat $name | awk 'Begin{i=0;flagEnd=0}{i++;if($1=="Collision"||$1=="Saving")flagEnd=1; if(i>11 && !flagEnd)printf("%f ",$6)}END{printf("\n")}'
-done
